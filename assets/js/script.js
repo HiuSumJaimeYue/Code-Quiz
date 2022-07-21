b16067e89cc3cdb4e49a57c76f1986baf3bae80c
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-07-21 15:22:22 -0400

    Showing options of question correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,62 +4,35 @@ var guideEl = document.querySelector("#guide");
 var startBtnEl = document.querySelector("#start-btn");
 
 var optionEl = document.querySelector("#choice-list");
-var optionCounter = 0;
+var optionCounter = 1;
 
-// var favoriteEl = document.createElement("div");
-// Create ordered list element
-// var listEl = document.createElement("ol");
+//Create HashMap
+var qAndA = ["Commonly used data types DO NOT Include:",
+"The condition in an if/else statement is enclosed with ___."
+];
 
-var questions = ["Commonly used data types DO NOT Include:"];
-
-var favoriteFood = ["1","2","3","4a"];
-// favoriteEl.textContent = "My favorite foods are:";
-// var colours = ["yellow", "pink", "blue","green"];
+var options = [["strings", "booleans", "alerts", "numbers"], 
+["quotes", "curly brackets", "parenthesis", "square brackets"]
+];
 
 function changeQuestion(){
     console.log(questionEl);
 
     // set new question
-    questionEl.textContent = questions[0];
+    questionEl.textContent = qAndA[0];
     questionEl.setAttribute("style", "display: inline-block; text-align: left;");
     startBtnEl.style.display = 'none';
     guideEl.style.display = 'none';
 
-        // var item = document.createElement("li");
-        // newText = document.createTextNode('txt');
-        // item.appendChild(newText);
-        // // item.textContent = "Hi";
-        // // item.appendChild(document.createTextNode("Four"));
-        // optionEl.appendChild(item);
-
-    // for (var i = 0; i < favoriteFood.length; i++){
-    //     var item = document.createElement("li");
-    //     item.textContent = favoriteFood[i];
-    //     item.setAttribute("id", optionCounter);
-    //     optionEl.appendChild(item);
-    //     optionCounter++;
-    //     item.className = "btn";
-    // }
-
-//         var item = document.createElement("button");
-//         item.textContent = favoriteFood[0];
-//         item.setAttribute("id", optionCounter);
-//         item.className = "btn-2";
-//         optionEl.appendChild(item);
-//         optionCounter++;
-// }
-
-    for (var i = 0; i < favoriteFood.length; i++){
+    for (var i = 0; i < options[0].length; i++){
         var item = document.createElement("li");
-        item.textContent = favoriteFood[i];
+        item.textContent = optionCounter + ". "+ options[0][i];
         item.setAttribute("id", optionCounter);
         optionEl.appendChild(item);
         optionCounter++;
         item.className = "btn-2";
     }
 }
-// favoriteEl.appendChild(listEl);
-// datesContainerEl.appendChild(favoriteEl);
 
 startBtnEl.addEventListener("click", changeQuestion);
 
